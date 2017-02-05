c81aae3cb0fc200d05c5032da8a9df8eb00b4e21
Abel Tay <abel.tay@gmail.com>
2017-02-05 16:50:34 +0800

    Add index page and 2 pages

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/index", Index)
+	mux.HandleFunc("/1", One)
+	mux.HandleFunc("/2", Two)
+	// mux.Handle("/static",http.FileServer(http.Dir("./static")))
+
+	s := &http.Server{
+		Addr:         ":8000",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	log.Println("Starting server...")
+	log.Fatal(s.ListenAndServe())
+}
---
+++ routes.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templateDir = "template/"
+
+var templates = template.Must(template.ParseFiles(
+	templateDir+"index.tmpl",
+	templateDir+"1.tmpl",
+	templateDir+"2.tmpl",
+	templateDir+"header.tmpl",
+	templateDir+"footer.tmpl"))
+
+type Page struct {
+	Title string
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	p := &Page{
+		Title: "Home",
+	}
+	renderTemplate(w, "index", p)
+}
+
+func One(w http.ResponseWriter, r *http.Request) {
+	p := &Page{
+		Title: "1",
+	}
+	renderTemplate(w, "1", p)
+}
+
+func Two(w http.ResponseWriter, r *http.Request) {
+	p := &Page{
+		Title: "2",
+	}
+	renderTemplate(w, "2", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data *Page) {
+	err := templates.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
